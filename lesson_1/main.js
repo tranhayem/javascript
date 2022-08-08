f3e0f32e68430af162ccfd0ee5f033a5796dbe78
Trần Ngọc Dương <lotaynaongo@gmail.com>
2022-08-08 20:28:02 +0700

    up

---
+++ lesson_1/main.js
@@ -0,0 +1,30 @@
+// Comment trong js
+// 1. Muc dich su dung 
+// - ghi chu
+// - vo hieu hoa ma 
+// 2. Loai Comments
+// - Comment 1 dong
+// - Comment nhieu dong
+// 3. Su dung phim tat
+// - window: ctrl + /
+// - macos: comman + /
+/**
+ * gioi thieu mot so ham built-in
+ * 1. Alert
+ * 2. Console
+ * 3. Confirm
+ * 4. Prompt
+ * 5. Set timeout- chay 1 doan code sau 1 khoang thoi gian
+ * 6. Set interval- chay dinh ky 1 doan code sau 1 khoang thoi gian
+ */
+
+/**
+ * toan tu trong javascript
+ * 1. toan tu so hoc- arithmetic( cac phep tinh +; -; *; **; /; %; ++; --)
+ * 2. toan tu gan- assignment( =; +=; -=; *=; /=; **=)
+ * 3. toan tu so sanh- comparison( >; <; ==)
+ * 4. toan tu logic- logical( &&)
+ */
+ 
+
+ 
