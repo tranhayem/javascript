1a89956c22e39daadd059cdb502976b4f731911c
Trần Ngọc Dương <lotaynaongo@gmail.com>
2022-08-09 22:24:25 +0700

    update

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,30 +1,30 @@
 // Comment trong js
-// 1. Muc dich su dung 
-// - ghi chu
-// - vo hieu hoa ma 
-// 2. Loai Comments
-// - Comment 1 dong
-// - Comment nhieu dong
-// 3. Su dung phim tat
+// 1. Mục đích sử dụng
+// - ghi chú
+// - vô hiệu hóa mã 
+// 2. Loại Comments
+// - Comment 1 dòng
+// - Comment nhiều dòng
+// 3. Sử dụng phím tắt
 // - window: ctrl + /
 // - macos: comman + /
 
 /**
- * gioi thieu mot so ham built-in
+ * giới thiệu một số hàm built-in
  * 1. Alert
  * 2. Console
  * 3. Confirm
  * 4. Prompt
- * 5. Set timeout- chay 1 doan code sau 1 khoang thoi gian
- * 6. Set interval- chay dinh ky 1 doan code sau 1 khoang thoi gian
+ * 5. Set timeout- chạy 1 đoạn code sau 1 khoảng thời gian
+ * 6. Set interval- chạy định kỳ 1 đoạn code sau 1 khoảng thời gian
  */
 
 /**
  * toan tu trong javascript
- * 1. toan tu so hoc- arithmetic( cac phep tinh +; -; *; **; /; %; ++; --)
- * 2. toan tu gan- assignment( =; +=; -=; *=; /=; **=)
- * 3. toan tu so sanh- comparison( ==; !=; >; <; >=; <=)
- * 4. toan tu logic- logical( &&; ||; !)
+ * 1. toán tử số học- arithmetic( cac phep tinh +; -; *; **; /; %; ++; --)
+ * 2. toán tử gán- assignment( =; +=; -=; *=; /=; **=)
+ * 3. toán tử so sánh- comparison( ==; !=; >; <; >=; <=; so sánh tuyệt đối ===; !==)
+ * 4. toán tử logic- logical( &&; ||; !)
  */
 
 /**
@@ -40,7 +40,7 @@ console.log(isSuccess);
  */
 
 /**
- * cac gia tri trong js
+ * các giá trị js
  * 0
  * false
  * '' -""
@@ -49,7 +49,26 @@ console.log(isSuccess);
  * null
  */
 
-var a= 1;
+/**
+ * kiểu dữ liệu trong js
+ * 1. dữ liệu nguyên thủy- primitive data
+ *  number- số
+ *  string- chuỗi( ''; "")
+ *  boolean- on hoặc off( true; false)
+ *  underfined- không gán giá trị( vd: var age;)
+ *  null- gán giá trị bằng null;
+ *  symbol- gán giá trị bằng Symbol(' ');
+ * 2. dữ liệu phức tạp- complex data
+ *  function
+ *  object- lưu thông tin
+ *          var myObject = { name:'', age: ,...};
+ *          var myArray = ['','','',...];
+ * 3. kiểm tra kiểu dữ liệu
+ *  console.log(typeof );
+ *  đặc biệt: console.log(typeof null); = object
+ */
+
+var a=1;
 var b=2;
 var c=3;
 if(a>0 && b >0){
